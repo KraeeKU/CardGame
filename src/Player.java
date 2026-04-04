72e22980202271cad88fe9047333d184542e2e46
KraeeKU <kevinuka88@gmail.com>
2026-04-04 02:06:51 +0100

    Remove unused fields from Player class
    Removed unused fields 'name', 'score', and 'streak' from Player class.

--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,5 @@
 public class Player {
-    private String name;
-    private int score = 0;
-    private int streak = 0;
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
