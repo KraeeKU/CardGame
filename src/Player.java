e7cac8da464657d28f843c7da83d12ad794fa2cf
KraeeKU <kevinuka88@gmail.com>
2026-04-04 01:53:26 +0100

    Refactor Player class to include main method
    Removed player methods and added main method for testing.

--- src/Player.java
+++ src/Player.java
@@ -3,32 +3,8 @@ public class Player {
     private int score = 0;
     private int streak = 0;
 
-    public Player(String name) {
-        this.name = name;
-    }
-
-    public void increaseScore() {
-        score++;
-    }
-
-    public void increaseStreak() {
-        streak++;
-    }
-
-    public void resetStreak() {
-        streak = 0;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public int getStreak() {
-        return streak;
-    }
-
-    public String getName() {
-        return name;
+    public static void main(String args[]){
+        system.out.println("Hello World");
     }
 }
 
